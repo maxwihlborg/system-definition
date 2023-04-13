0045e539b35e88674058c3b80998d7191bff4aa5
Max Wihlborg <6181962+maxwihlborg@users.noreply.github.com>
2023-04-13 05:36:28 +0200

    git stuff

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -3,6 +3,7 @@
     ripgrep
     # fish
     nushell
+    delta
     sad
     exa
     fd
@@ -26,6 +27,67 @@
     # l10n
     LANG = "en_US.UTF-8";
     LC_ALL = "en_US.UTF-8";
+  };
+
+  programs.git = {
+    enable = true;
+    userEmail = "6181962+maxwihlborg@users.noreply.github.com";
+    userName = "Max Wihlborg";
+    aliases = {
+      rename = "!moveit() { git push origin --delete `git branch --show-current` || true; git branch -m $1; git push --set-upstream origin $1; }; moveit";
+      co = "checkout";
+      ci = "commit";
+      st = "status";
+      br = "branch";
+      rb = "rebase";
+    };
+    extraConfig = {
+      init = { defaultBranch = "main"; };
+      push = { default = "simple"; };
+      pull = { rebase = true; };
+      fetch = { prune = true; };
+    };
+    ignores = [
+      # General
+      ".DS_Store"
+      ".AppleDouble"
+      ".LSOverride"
+
+      # Icon must end with two \r
+      "Icon\r\r"
+
+      # Thumbnails
+      "._*"
+
+      # Files that might appear in the root of a volume
+      ".DocumentRevisions-V100"
+      ".fseventsd"
+      ".Spotlight-V100"
+      ".TemporaryItems"
+      ".Trashes"
+      ".VolumeIcon.icns"
+      ".com.apple.timemachine.donotpresent"
+
+      # Directories potentially created on remote AFP share
+      ".AppleDB"
+      ".AppleDesktop"
+      "Network Trash Folder"
+      "Temporary Items"
+      ".apdisk"
+    ];
+  };
+
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "${pkgs.delta}/bin/delta --dark --paging=never --24-bit-color=never";
+          branchLogCmd = "git log --graph --color=always --abbrev-commit --decorate --date=relative --pretty=medium --oneline {{branchName}} --";
+        };
+      };
+    };
   };
 
   programs.fish = {
