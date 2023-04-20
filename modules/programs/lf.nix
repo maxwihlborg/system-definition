30063e5644115724c8a778f72048e290af1c2346
Max Wihlborg <6181962+maxwihlborg@users.noreply.github.com>
2023-04-20 22:11:36 +0200

    dynamic lf config

--- modules/programs/lf.nix
+++ modules/programs/lf.nix
@@ -1,7 +1,10 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+in
 {
   home.packages = [ pkgs.lf ];
-  xdg.configFile."lf/lfrc" = {
-    source = ../../config/lf/lfrc;
+  xdg.configFile."lf" = {
+    source = mkOutOfStoreSymlink "${config.home.homeDirectory}/ghq/github.com/maxwihlborg/system-definition/config/lf";
   };
 }
