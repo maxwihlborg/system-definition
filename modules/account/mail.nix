9dc2a455f2079ac885470ab83d2c73b0edf65f16
Max Wihlborg <6181962+maxwihlborg@users.noreply.github.com>
2024-11-14 03:35:37 +0100

    feat(neomutt): configure email

--- modules/account/mail.nix
+++ modules/account/mail.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.notmuch = {
     enable = true;
@@ -10,6 +10,10 @@
   programs.neomutt = {
     enable = true;
     vimKeys = true;
+    sort = "reverse-date";
+    sidebar = {
+      enable = true;
+    };
   };
 
   accounts.email =
@@ -23,7 +27,29 @@
         address = mail.address.wtr;
         userName = mail.address.wtr;
         flavor = "gmail.com";
-        notmuch.enable = true;
+
+        neomutt = {
+          enable = true;
+          sendMailCommand = "gmi send -t -C ${config.accounts.email.maildirBasePath}/wtr";
+          showDefaultMailbox = false;
+          extraConfig = ''
+            set virtual_spoolfile = yes
+          '';
+        };
+
+        notmuch = {
+          enable = true;
+          neomutt = {
+            enable = true;
+            virtualMailboxes = [
+              { name = "INBOX"; query = "folder:/wtr/ tag:inbox"; }
+              { name = "Unread"; query = "folder:/wtr/ tag:unread"; }
+              { name = "Archive"; query = "folder:/wtr/ tag:archive"; }
+              { name = "Sent"; query = "folder:/wtr/ tag:sent"; }
+              { name = "Trash"; query = "folder:/wtr/ tag:trash"; }
+            ];
+          };
+        };
 
         lieer.enable = true;
       };
